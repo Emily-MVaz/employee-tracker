e67940b3f5d9335f53bc44211e4425f191b51ad0
Emily Vazquez <emily.m.vazquez99@gmail.com>
2022-04-07 19:26:01 -0500

    fixed schema tables

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,7 @@ USE employees;
 -- name: VARCHAR(30) to hold department name
 CREATE TABLE department (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    department_name  VARCHAR(30) NOT NULL
+    department_name VARCHAR(30)
 );
 
 
@@ -18,7 +18,7 @@ CREATE TABLE department (
 -- department_id: INTEGER to hold reference to department role belongs to
 CREATE TABLE role (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL,
+    title VARCHAR(30),
     salary DECIMAL(10,2) NOT NULL,
     department_id INTEGER
 );
@@ -31,8 +31,8 @@ CREATE TABLE role (
 -- manager_id: INTEGER to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
 CREATE TABLE employee (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
     role_id INTEGER,
     manager_id INTEGER
 );
